e85b2fa34cb6a271c8efb76332698eecf3c5ecb9
Carlos Blanco <carlosblanco@me.com>
2020-12-15 19:37:06 +0100

    Adding event card component

---
+++ src/components/EventCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="event-card">
+    <h1>{{ event.name }}</h1>
+    <p>{{ event.description }}</p>
+    <pre>{{ event.date }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.event-card {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 1rem;
+  display: inline-block;
+  width: 500px;
+  h1 {
+    color: #41b883;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 1,
+          name: 'Aprender Vue en VueMastery',
+          date: '5-12-2020',
+          description: 'Apender Real Word Vue antes de finalizar Diciembre'
+        },
+        {
+          id: 2,
+          name: 'Hacer ejercicio',
+          date: '01-01-2021',
+          description: 'New Year resolution'
+        },
+        {
+          id: 3,
+          name: 'Comer mejor',
+          date: '01-01-2021',
+          description: 'New Year resolution'
+        }
+      ]
+    }
   }
-};
+}
 </script>
+
+<style lang="scss">
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
